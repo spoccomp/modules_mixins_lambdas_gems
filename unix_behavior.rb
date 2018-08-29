6d2b0ba074a9444814e4fa929ed2e8f61b6b359e
richard trapanese <richardtrapanese@gmail.com>
2018-08-29 09:57:45 -0400

    updated files and tested good to go

--- unix_behavior.rb
+++ unix_behavior.rb
@@ -1,12 +1,13 @@
 module UnixBehavior
- #mac
+
+
  def boot_up
     @booted = true
-  end
-
-  def shut_down
-    @booted = false
-  end
+ end
+    
+ def shut_down
+     @booted = false
+ end
 
   def version_name
     "OSX version: #{ @version } : #{ @codename }"
@@ -15,14 +16,7 @@ module UnixBehavior
   def load_terminal
     "#{ @name }:~ $ "
   end
-  #linux
-  def boot_up
-    @booted = true
-  end
-
-  def shut_down
-    @booted = false
-  end
+  
 
   def version_name
     "Ubuntu version: #{ @version } : #{ @codename }"
